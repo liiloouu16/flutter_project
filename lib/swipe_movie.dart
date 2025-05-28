c8568f71bf18c0a792c3741f3687b7c0e309aa96
AtoupikK <edwyn.piquet@gmail.com>
2025-05-28 09:53:25 +0200

    Update swipe_movie.dart

--- lib/swipe_movie.dart
+++ lib/swipe_movie.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:convert'; //pour décoder la réponse json
 import 'package:http/http.dart' as http; //pour les requêtes http
 import 'package:scrumlab_flutter_tindercard/scrumlab_flutter_tindercard.dart';
-import 'botNavBar.dart';
+import 'bot_nav_bar.dart';
 
 class SwipeMovie extends StatefulWidget {
   const SwipeMovie({super.key});
@@ -10,8 +10,7 @@ class SwipeMovie extends StatefulWidget {
   State<SwipeMovie> createState() => _SwipeMovieState();
 }
 
-class _SwipeMovieState extends State<SwipeMovie>{
-
+class _SwipeMovieState extends State<SwipeMovie> {
   //liste d'images récupérées depuis l'API
   List<Map<String, String>> movieDetails = [];
   List<Map<String, String>> lastSwiped = [];
@@ -22,15 +21,15 @@ class _SwipeMovieState extends State<SwipeMovie>{
   CardController controller = CardController();
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     //appel initial pour récuperer tous les films/séries
     fetchAll();
   }
 
   //fonction qui récupère tout (film+série)
-  Future<void> fetchAll() async{
-    try{
+  Future<void> fetchAll() async {
+    try {
       //liste de toutes les catégories
       final allCategories = [
         "movie/popular",
@@ -60,56 +59,60 @@ class _SwipeMovieState extends State<SwipeMovie>{
   }
 
   //fonction qui récupère les films
-  Future<List<Map<String, String>>> fetchMovies(String category) async{
-    try{
+  Future<List<Map<String, String>>> fetchMovies(String category) async {
+    try {
       //construction de l'url
-      final String url = "https://api.themoviedb.org/3/$category?api_key=$apiKey&language=fr-FR&page=1";
+      final String url =
+          "https://api.themoviedb.org/3/$category?api_key=$apiKey&language=fr-FR&page=1";
 
       //envoie de la requête GET
       final response = await http.get(Uri.parse(url));
 
       //si aucune réponse est retournée
       if (response.statusCode != 200) {
-        throw Exception("Erreur ${response.statusCode} : ${response.reasonPhrase}");
+        throw Exception(
+            "Erreur ${response.statusCode} : ${response.reasonPhrase}");
       }
 
       //sinon décode la réponse
       final dataUrl = json.decode(response.body);
 
       //informations des films
-      List<Map<String,String>> movies = await Future.wait(
+      List<Map<String, String>> movies = await Future.wait(
         (dataUrl["results"] as List)
-          //évite erreur nulles
-          .where((movie) => movie["poster_path"] != null)
-          .map((movie) async => {
-            "image": "https://image.tmdb.org/t/p/original${movie["poster_path"]}",
-            "title": ( movie["title"] ?? movie["name"] ?? "Sans titre").toString(),
-            "release_date": (movie["release_date"] ?? movie["first_air_date"]).toString(),
-            "runtime": await fetchRuntime(movie["id"]),
-          })
-          .toList(),
+            //évite erreur nulles
+            .where((movie) => movie["poster_path"] != null)
+            .map((movie) async => {
+                  "image":
+                      "https://image.tmdb.org/t/p/original${movie["poster_path"]}",
+                  "title": (movie["title"] ?? movie["name"] ?? "Sans titre")
+                      .toString(),
+                  "release_date":
+                      (movie["release_date"] ?? movie["first_air_date"])
+                          .toString(),
+                  "runtime": await fetchRuntime(movie["id"]),
+                })
+            .toList(),
       );
 
       //mélange les images aléatoirement
       movies.shuffle();
 
       return movies;
-    }
-    catch(e){
+    } catch (e) {
       print("Erreur lors de la récupération des films : $e");
       return [];
     }
   }
 
   //fonction qui change l'url selon onglet selected
-  void updateUrl(int index) async{
+  void updateUrl(int index) async {
     String category;
     try {
       //si onglet "tout" cliqué
       if (index == 0) {
         await fetchAll();
-      }
-      else {
+      } else {
         switch (index) {
           case 1:
             category = "movie/now_playing";
@@ -128,16 +131,17 @@ class _SwipeMovieState extends State<SwipeMovie>{
           movieDetails = newImages;
         });
       }
-    }catch(e){
+    } catch (e) {
       print("Erreur lors de la mise à jour des films : $e");
     }
   }
 
   //récupère la durée du film
-  Future<String> fetchRuntime (int movieId) async{
+  Future<String> fetchRuntime(int movieId) async {
     try {
       //création de la requête
-      final String url = "https://api.themoviedb.org/3/movie/$movieId?api_key=$apiKey&language=fr-FR";
+      final String url =
+          "https://api.themoviedb.org/3/movie/$movieId?api_key=$apiKey&language=fr-FR";
 
       //envoie de la requête GET
       final response = await http.get(Uri.parse(url));
@@ -150,7 +154,7 @@ class _SwipeMovieState extends State<SwipeMovie>{
 
       //sinon décode la réponse
       final dataUrl = json.decode(response.body);
-      int runtime =  dataUrl["runtime"] ?? 0;
+      int runtime = dataUrl["runtime"] ?? 0;
 
       if (runtime == 0) return "Durée inconnue ici";
 
@@ -160,11 +164,9 @@ class _SwipeMovieState extends State<SwipeMovie>{
 
       //si la conversion est nécéssaire
       return hours > 0
-        ? "$hours h ${minutes.toString().padLeft(2, '0')} min"
-        : "$minutes min";
-
-
-    } catch(e){
+          ? "$hours h ${minutes.toString().padLeft(2, '0')} min"
+          : "$minutes min";
+    } catch (e) {
       print("Erreur lors de la récupération des runtime : $e");
       return "Durée inconnue";
     }
@@ -193,9 +195,10 @@ class _SwipeMovieState extends State<SwipeMovie>{
         ),
         //contenu en dessous de l'onglet
         body: Padding(
-          padding: const EdgeInsets.only(top:20),
+          padding: const EdgeInsets.only(top: 20),
           child: TabBarView(
-            physics: NeverScrollableScrollPhysics(),//enlève le défilement entre les tabs
+            physics:
+                NeverScrollableScrollPhysics(), //enlève le défilement entre les tabs
             children: [
               buildMovieList(),
               buildMovieList(),
@@ -209,152 +212,164 @@ class _SwipeMovieState extends State<SwipeMovie>{
     );
   }
 
-
   //affichage
-  Widget buildMovieList(){
+  Widget buildMovieList() {
     return Center(
       //chargement
-      child: isLoading ? const CircularProgressIndicator()
-      : movieDetails.isEmpty? const Text("Aucun film trouvé", style: TextStyle(fontSize: 18),)
-        //Moviecard
-        : Column(
-          children: [
-            SizedBox(
-                height: MediaQuery.of(context).size.height * 0.65,
-                child: TinderSwapCard(
-                  swipeUp: false,
-                  swipeDown: false,
-                  orientation: AmassOrientation.top,
-                  totalNum: movieDetails.length,
-                  stackNum: 3,
-                  swipeEdge: 3.0,
-                  maxWidth: MediaQuery.of(context).size.width * 0.85,
-                  maxHeight: MediaQuery.of(context).size.height * 0.7,
-                  minWidth: MediaQuery.of(context).size.width * 0.75,
-                  minHeight: MediaQuery.of(context).size.height * 0.6,
-                  cardBuilder: (context, index) => Card(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start, //aligner à gauche
-                      children: [
-                        Expanded(child:
-                          Image.network(
-                            movieDetails[index]["image"]!,
-                            fit: BoxFit.cover,
-                            width: double.infinity,//éviter bord blanc
+      child: isLoading
+          ? const CircularProgressIndicator()
+          : movieDetails.isEmpty
+              ? const Text(
+                  "Aucun film trouvé",
+                  style: TextStyle(fontSize: 18),
+                )
+              //Moviecard
+              : Column(
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.65,
+                      child: TinderSwapCard(
+                        swipeUp: false,
+                        swipeDown: false,
+                        orientation: AmassOrientation.top,
+                        totalNum: movieDetails.length,
+                        stackNum: 3,
+                        swipeEdge: 3.0,
+                        maxWidth: MediaQuery.of(context).size.width * 0.85,
+                        maxHeight: MediaQuery.of(context).size.height * 0.7,
+                        minWidth: MediaQuery.of(context).size.width * 0.75,
+                        minHeight: MediaQuery.of(context).size.height * 0.6,
+                        cardBuilder: (context, index) => Card(
+                          child: Column(
+                            crossAxisAlignment:
+                                CrossAxisAlignment.start, //aligner à gauche
+                            children: [
+                              Expanded(
+                                child: Image.network(
+                                  movieDetails[index]["image"]!,
+                                  fit: BoxFit.cover,
+                                  width: double.infinity, //éviter bord blanc
+                                ),
+                              ),
+                              //TITRE
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 15.0, top: 7.0, bottom: 1),
+                                child: Text(
+                                  movieDetails[index]["title"] ??
+                                      "Titre inconnu",
+                                  style: const TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              //DATE
+                              Padding(
+                                padding: const EdgeInsets.only(left: 15.0),
+                                child: Text(
+                                  "Date de sortie : ${movieDetails[index]["release_date"] ?? "Date inconnue"}",
+                                  style: const TextStyle(
+                                      fontSize: 16, color: Colors.grey),
+                                ),
+                              ),
+                              //DUREE
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 15.0, bottom: 7),
+                                child: Text(
+                                  "Durée : ${movieDetails[index]["runtime"] ?? "Durée inconnue"}",
+                                  style: const TextStyle(
+                                      fontSize: 16, color: Colors.grey),
+                                ),
+                              )
+                            ],
                           ),
                         ),
-                        //TITRE
-                        Padding(
-                          padding: const EdgeInsets.only(left: 15.0, top: 7.0, bottom: 1),
-                          child: Text(
-                            movieDetails[index]["title"] ?? "Titre inconnu",
-                            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        //DATE
-                        Padding(
-                          padding: const EdgeInsets.only(left: 15.0),
-                          child: Text(
-                            "Date de sortie : ${movieDetails[index]["release_date"] ?? "Date inconnue"}",
-                            style: const TextStyle(fontSize: 16, color: Colors.grey),
-                          ),
-                        ),
-                        //DUREE
-                        Padding(padding: const EdgeInsets.only(left: 15.0, bottom: 7),
-                          child: Text(
-                            "Durée : ${movieDetails[index]["runtime"] ?? "Durée inconnue"}",
-                            style: const TextStyle(fontSize: 16, color: Colors.grey),
-                          ),
-                        )
-                      ],
+                        cardController: controller,
+                        swipeCompleteCallback:
+                            (CardSwipeOrientation orientation, int index) {
+                          if (orientation == CardSwipeOrientation.left ||
+                              orientation == CardSwipeOrientation.right) {
+                            setState(() {
+                              lastSwiped.clear();
+                              print("ici");
+                              print(lastSwiped);
+                              lastSwiped.add(movieDetails[index]);
+                              print("la");
+                              print(lastSwiped);
+                            });
+                          }
+                        },
+                      ),
                     ),
-                  ),
-                  cardController: controller,
-                  swipeCompleteCallback: (CardSwipeOrientation orientation, int index) {
-                    if (orientation == CardSwipeOrientation.left || orientation == CardSwipeOrientation.right) {
-                      setState(() {
-                        lastSwiped.clear();
-                        print("ici");
-                        print(lastSwiped);
-                        lastSwiped.add(movieDetails[index]);
-                        print("la");
-                        print(lastSwiped);
-                      });
-                    }
-                  },
-                ),
-            ),
-            SizedBox(height: 20),
-            //BOUTTONS
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                //RETOUR
-                IconButton(
-                  icon: const Icon(
-                    Icons.rotate_left,
-                    color: Colors.amber,
-                    size: 40,
-                  ),
-                  onPressed: (){
-                    if (lastSwiped.isNotEmpty) {
-                      setState(() {
-                        movieDetails.insert(0, lastSwiped.last);
-                        lastSwiped.removeLast();
-                      });
-                      controller.triggerLeft();
-                    }
-                  },
-                  style: IconButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50),
-                      side: const BorderSide(color: Color(0xFFEAEAEA), width:1.7)
+                    SizedBox(height: 20),
+                    //BOUTTONS
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        //RETOUR
+                        IconButton(
+                          icon: const Icon(
+                            Icons.rotate_left,
+                            color: Colors.amber,
+                            size: 40,
+                          ),
+                          onPressed: () {
+                            if (lastSwiped.isNotEmpty) {
+                              setState(() {
+                                movieDetails.insert(0, lastSwiped.last);
+                                lastSwiped.removeLast();
+                              });
+                              controller.triggerLeft();
+                            }
+                          },
+                          style: IconButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50),
+                                  side: const BorderSide(
+                                      color: Color(0xFFEAEAEA), width: 1.7))),
+                        ),
+                        SizedBox(width: 20),
+                        //FAVORIS
+                        IconButton(
+                          icon: const Icon(
+                            Icons.favorite_rounded,
+                            color: Colors.green,
+                            size: 55,
+                          ),
+                          onPressed: () {
+                            controller.triggerRight();
+                          },
+                          style: IconButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50),
+                                  side: const BorderSide(
+                                      color: Color(0xFFEAEAEA), width: 1.7))),
+                        ),
+                        SizedBox(width: 20),
+                        //SKIP
+                        IconButton(
+                          icon: const Icon(
+                            Icons.clear,
+                            color: Colors.red,
+                            size: 40,
+                          ),
+                          onPressed: () {
+                            controller.triggerLeft();
+                          },
+                          style: IconButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50),
+                                  side: const BorderSide(
+                                      color: Color(0xFFEAEAEA), width: 1.7))),
+                        ),
+                      ],
                     )
-                  ),
+                  ],
                 ),
-                SizedBox(width: 20),
-                //FAVORIS
-                IconButton(
-                  icon: const Icon(
-                    Icons.favorite_rounded,
-                    color: Colors.green,
-                    size: 55,
-                  ),
-                  onPressed: (){
-                    controller.triggerRight();
-                  },
-                  style: IconButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50),
-                          side: const BorderSide(color: Color(0xFFEAEAEA), width:1.7)
-                      )
-                  ),
-                ),
-                SizedBox(width: 20),
-                //SKIP
-                IconButton(
-                  icon: const Icon(
-                    Icons.clear,
-                    color: Colors.red,
-                    size: 40,
-                  ),
-                  onPressed: (){
-                    controller.triggerLeft();
-                  },
-                  style: IconButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50),
-                          side: const BorderSide(color: Color(0xFFEAEAEA), width:1.7)
-                      )
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
     );
   }
 }
